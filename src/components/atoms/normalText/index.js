72cacd19a7d2e8b8b1cb774400aad7e63f93c193
prakash chandra muduli <prakashchandramuduli007@gmail.com>
2023-02-11 03:01:31 +0530

    home screen all section added

---
+++ src/components/atoms/normalText/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./normalText.css";
+function NormalText({ children }) {
+  return <h3 className="normalText">{children}</h3>;
+}
+
+export default NormalText;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,20 +2,26 @@ import React from "react";
 import "./home.css";
 import CustomButton from "../../components/atoms/CustomButton";
 import img from "./img/logos.jpg";
-import sideImg from "./sideImg/sideimg.jpg";
+// import sideImg from "./sideImg/sideimg.jpg";
 import Heading from "../../components/atoms/headingText";
 import SubHeading from "../../components/atoms/SubHeading";
 import SmallHeading from "../../components/atoms/SmallHeading";
 import MedsizeHeading from "../../components/atoms/MedsizeHeading";
 import opncls from "./coursesectionImg/opncls.jpg";
+import dotedImg from "../../asserts/images/dotedImg.png";
 import rightside from "./courseSecRight/rightside.jpg";
+import codeImg from "../../asserts/images/svgs/codeImg.svg";
+import circleImg from "../../asserts/images/svgs/circle.svg";
+import doted from "../../asserts/images/svgs/dotedImg.svg";
+import sideImg from "../../asserts/images/svgs/heroSecImg.svg";
+import { Flex } from "@chakra-ui/react";
 
 const Home = () => {
   return (
     <div className="homeContainer">
       <div className="heroSection">
         <div className="heroSectionleft">
-          <Heading styles={{ color: "black" }}>
+          <Heading styles={{ color: "black", minWidth: 700 }}>
             Educative Accelerates Developer Productivity
           </Heading>
           <SubHeading styles={{ marginTop: 30, marginBottom: 30 }}>
@@ -65,14 +71,30 @@ const Home = () => {
             different. Practice as you learn with live code environments inside
             your browser.
           </SubHeading>
-          <img src={opncls} alt="</>" className="opnclsImg" />
-          <SmallHeading styles={{ color: "white" }}>
-            Hands-on Projects: Build Real World Applications
-          </SmallHeading>
-          <img src={opncls} alt="</>" className="opnclsImg" />
-          <SmallHeading styles={{ color: "white" }}>
-            Hands-on Projects: Build Real World Applications
-          </SmallHeading>
+          <Flex>
+            <div>
+              <img
+                src={opncls}
+                style={{ width: "10%", height: "20%" }}
+                alt="</>"
+                className="opnclsImg"
+              />
+              <SmallHeading styles={{ color: "white" }}>
+                Hands-on Projects: Build Real World Applications
+              </SmallHeading>
+            </div>
+            <div>
+              <img
+                src={opncls}
+                style={{ width: "10%", height: "20%" }}
+                alt="</>"
+                className="opnclsImg"
+              />
+              <SmallHeading styles={{ color: "white" }}>
+                Hands-on Projects: Build Real World Applications
+              </SmallHeading>
+            </div>
+          </Flex>
         </div>
 
         <div className="courseSection_right">
@@ -83,15 +105,15 @@ const Home = () => {
           />
         </div>
         {/* downsection of course section */}
-        <div className="courseSection_left">
+        {/* <div className="courseSection_left">
           <img
             src={rightside}
             alt="rightsideCourseSection"
             className="rightsidecourseimg"
           />
-        </div>
+        </div> */}
 
-        <div className="courseSection_left">
+        {/* <div className="courseSection_left">
           <ul
             style={{
               color: "rgb(126,128,249)",
@@ -126,12 +148,123 @@ const Home = () => {
           <SmallHeading styles={{ color: "white" }}>
             Personalized Paths
           </SmallHeading>
+        </div> */}
+      </div>
+      <div className="courseSection">
+        <div className="courseSection_left">
+          <img
+            src={rightside}
+            alt="rightsideCourseSection"
+            className="rightsidecourseimg"
+          />
+        </div>
+        <div className="courseSection_right">
+          <ul
+            style={{
+              color: "rgb(126,128,249)",
+              marginTop: 30,
+              paddingLeft: 20,
+              paddingBottom: 20,
+            }}
+          >
+            <li>2x FASTER LEARNING</li>
+          </ul>
+          <MedsizeHeading styles={{ color: " white" }}>
+            2x FASTER LEARNING
+          </MedsizeHeading>
+          <SubHeading
+            styles={{
+              color: "white",
+              paddingleft: "50%",
+              marginTop: 30,
+              marginBottom: 30,
+            }}
+          >
+            Reading is faster than watching: The average tutorial is spoken at
+            150 words per minute. You can read at 250. That’s why our courses
+            are laid out as rich text and an interactive experience so you learn
+            more, learn faster, and learn exactly what you need.
+          </SubHeading>
+          <Flex>
+            <div>
+              <img
+                src={opncls}
+                style={{ width: "10%", height: "20%" }}
+                alt="</>"
+                className="opnclsImg"
+              />
+              <SmallHeading styles={{ color: "white" }}>
+                Hands-on Projects: Build Real World Applications
+              </SmallHeading>
+            </div>
+            <div>
+              <img
+                src={opncls}
+                style={{ width: "10%", height: "20%" }}
+                alt="</>"
+                className="opnclsImg"
+              />
+              <SmallHeading styles={{ color: "white" }}>
+                Hands-on Projects: Build Real World Applications
+              </SmallHeading>
+            </div>
+          </Flex>
         </div>
       </div>
-
+      <div className="getStatedSection">
+        <SubHeading
+          styles={{
+            color: "white",
+            paddingleft: "50%",
+            marginTop: 30,
+            marginBottom: 30,
+          }}
+        >
+          Supercharge Your Development Career
+        </SubHeading>
+        <CustomButton width={150} height={40}>
+          Start Free Trail
+        </CustomButton>
+        <img src={doted} alt="dotedImg" className="dotedImg" />
+      </div>
       <div className="feedBackSection">
-        <div className="feedBackSectionleft"></div>
-        <div className="feedBackSectionRight"></div>
+        <img src={circleImg} alt="circleImg" className="circleImg" />
+        <div
+          style={{
+            width: "80%",
+            height: "50vh",
+            borderRadius: 12,
+            borderWidth: "1px",
+            display: "flex",
+            borderColor: "#0e3458",
+            overflow: "hidden",
+            position: "relative",
+          }}
+        >
+          <img
+            src={doted}
+            alt="dotedImg"
+            style={{ position: "absolute", width: "70%", height: "100" }}
+          />
+          <div className="feedBackSectionleft">
+            <SubHeading
+              styles={{
+                color: "white",
+                paddingleft: "50%",
+                marginTop: 30,
+                marginBottom: 30,
+              }}
+            >
+              Join more than 1.7 Million Developers
+            </SubHeading>
+
+            <CustomButton width={150} height={40}>
+              Start Free Trail
+            </CustomButton>
+          </div>
+          <div className="feedBackSectionRight"></div>
+        </div>
+        <img alt="codeImg" src={codeImg} className="feedBackImg" />
       </div>
     </div>
   );
