c3060744199e8c11215d1f930aaf3b6de1871746
Prashant Chavda <prashantchavda700@gmail.com>
2023-02-23 16:06:06 +0530

    profileHead

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,33 @@
 import React from "react";
+// import Image from "../asserts/images/imageprofilesec/imageprofile.png";
+import Image from "../../asserts/images/imageprofilesec/imageprofile.png";
 
-const Profile = () => {
+import Heading from "../../components/atoms/headingText";
+import SmallHeading from "../../components/atoms/SmallHeading";
+import "./profile.css";
+
+const Profile = ({ username, price }) => {
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="profileContainer">
+      <div className="profileHead">
+      <img src={Image} />
+        <div
+          className="profileNameCard"
+          style={{ minWidhth: "200", maxHeight: "200" }}
+        >
+        
+          <Heading>Welcome back,{username}!</Heading>
+          <SmallHeading>
+            Make today the day.Get courses from real-world experts from ₹{price}{" "}
+          </SmallHeading>
+        
+
+        </div>
+      </div>
+      <div className="profileMylearning">
+        <h1>Let's start leraning,{username}</h1>
+        <div className="profileCourse">jnjn</div>
+      </div>
     </div>
   );
 };
